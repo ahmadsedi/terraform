064b33ad1d42685fbba5443c1cd36d2a0abcf1e6
ahmadseddighi@gmail.com <ahmadseddighi@gmail.com>
2025-11-28 17:47:00 +0000

    Add infra_config directory

--- infra_config/imports.tf
+++ infra_config/imports.tf
@@ -4,45 +4,40 @@
 
 import {
   to = module.main.aws_vpc.this[0]
-  id = "VPC" #VPC
+  id = "vpc-0a8b4ce0df0cda0f1" #VPC
 }
 
 import {
   to = module.main.aws_subnet.public[0]
-  id = "PublicSubnet1" #PublicSubnet1
+  id = "subnet-0f67734d6da807dc4" #PublicSubnet1
 }
 
 import {
   to = module.main.aws_subnet.public[1]
-  id = "PublicSubnet2" #PublicSubnet2
+  id = "subnet-0244edffd826ff24e" #PublicSubnet2
 }
 
 import {
   to = module.main.aws_internet_gateway.this[0]
-  id = "InternetGateway" #InternetGateway
+  id = "igw-0953b7d997a59773e" #InternetGateway
 }
 
 import {
   to = module.main.aws_route.public_internet_gateway[0]
-  id = "DefaultPublicRoute" #DefaultPublicRoute
+  id = "rtb-07416df4efa490116_0.0.0.0/0" #DefaultPublicRoute
 }
 
 import {
   to = module.main.aws_route_table.public[0]
-  id = "PublicRouteTable" #PublicRouteTable
+  id = "rtb-07416df4efa490116" #PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[0]
-  id = "PublicSubnet1/PublicRouteTable" #PublicSubnet1/PublicRouteTable
+  id = "subnet-0f67734d6da807dc4/rtb-07416df4efa490116" #PublicSubnet1/PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[1]
-  id = "PublicSubnet2/PublicRouteTable" #PublicSubnet2/PublicRouteTable
-}
-
-import {
-  to = aws_security_group.ingress
-  id = "NoIngressSecurityGroup" #NoIngressSecurityGroup
+  id = "subnet-0244edffd826ff24e/rtb-07416df4efa490116" #PublicSubnet2/PublicRouteTable
 }
--- infra_config/resources.tf
+++ infra_config/resources.tf
@@ -40,3 +40,26 @@ module "main" {
 
   tags = local.common_tags
 }
+
+# __generated__ by Terraform from "sg-0e49b7ac47719d483"
+resource "aws_security_group" "ingress" {
+  description = "Security group with no ingress rule"
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = ""
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "-1"
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+  ingress                = []
+  name                   = "no-ingress-sg"
+  name_prefix            = null
+  revoke_rules_on_delete = null
+  tags                   = local.common_tags
+  tags_all               = {}
+  vpc_id                 = module.main.vpc_id
+}
